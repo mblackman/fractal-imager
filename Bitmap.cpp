7e28558a8281f3b3e420ec789eff9bfc32d9c2b4
Matt Blackman <matt.blackman@pocketgems.com>
2024-04-02 13:36:31 -0600

    Implement set pixel and test

--- Bitmap.cpp
+++ Bitmap.cpp
@@ -45,7 +45,12 @@ bool Bitmap::write(string filename) {
 }
 
 void Bitmap::setPixel(int x, int y, uint8_t red, uint8_t green, uint8_t blue) {
+    uint8_t *pPixel = m_pixels.get();
 
+    pPixel += y * 3 * m_width + x * 3;
+    pPixel[0] = blue;
+    pPixel[1] = green;
+    pPixel[2] = red;
 }
 
 Bitmap::~Bitmap() {
--- main.cpp
+++ main.cpp
@@ -7,7 +7,13 @@ using namespace fractal;
 int main() {
     Bitmap bitmap{400, 400};
 
-    bitmap.write("test.bin");
-    
+    for (int i = 100; i<=300; i++) {
+        for (int j = 100; j <=300; j++) {
+            bitmap.setPixel(i, j, i % 255, j % 255, (i + j) % 255);
+        }
+    }
+
+    bitmap.write("test.bmp");
+
     return 0;
 }
